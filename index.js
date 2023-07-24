2f96fafe573951a4c21cdcc1ad51b25a18c281bc
Ilia <51325702+treger78@users.noreply.github.com>
2023-07-24 14:16:27 +0300

    refactor: refactor listProductsByName & listProductsByProducer

--- index.js
+++ index.js
@@ -43,6 +43,20 @@ var ShopImpl = (function () {
     return false;
   };
 
+  const listProductsByProperty = (products, callback, property, searchString) => {
+    const foundProducts = [];
+    
+    for (let i = 0; i < products.length; i += 1) {
+      if (foundProducts.length === 10) return callback(foundProducts);
+           
+      if (products[i][property].includes(searchString)) {
+        foundProducts.push(products[i]);
+      }
+    }
+
+    return callback(foundProducts);
+  };
+
   /**
     - Returns: 10 product names containing the specified string. If there are several products with the same name, producer's name is appended to product's name.
    */
@@ -71,19 +85,7 @@ var ShopImpl = (function () {
       return foundProductNames;
     }
     
-    const foundProducts = [];
-    
-    for (let i = 0; i < products.length; i += 1) {
-      if (foundProducts.length === 10) return getFoundProductNames(foundProducts);
-      
-      const currentProduct = products[i];
-      
-      if (currentProduct.name.includes(searchString)) {
-        foundProducts.push(currentProduct);
-      }
-    }
-
-    return getFoundProductNames(foundProducts);
+    return listProductsByProperty(products, getFoundProductNames, 'name', searchString);
   };
 
   /**
@@ -108,55 +110,45 @@ var ShopImpl = (function () {
       return foundProductNames;
     }
     
-    const foundProducts = [];
-    
-    for (let i = 0; i < products.length; i += 1) {
-      if (foundProducts.length === 10) return sortFoundProductsByProducer(foundProducts);
-      
-      if (products[i].producer.includes(searchString)) {
-        foundProducts.push(products[i]);
-      }
-    }
-    
-    return sortFoundProductsByProducer(foundProducts);
+    return listProductsByProperty(products, sortFoundProductsByProducer, 'producer', searchString);
   };
 
   return ShopImpl;
 }());
 
 function test(shop) {
-    assert(!shop.deleteProduct("1"));
-    assert(shop.addNewProduct({ id: "1", name: "1", producer: "Lex" }));
-    assert(!shop.addNewProduct({ id: "1", name: "any name because we check id only", producer: "any producer" }));
-    assert(shop.deleteProduct("1"));
-    assert(shop.addNewProduct({ id: "3", name: "Some Product3", producer: "Some Producer2" }));
-    assert(shop.addNewProduct({ id: "4", name: "Some Product1", producer: "Some Producer3" }));
-    assert(shop.addNewProduct({ id: "2", name: "Some Product2", producer: "Some Producer2" }));
-    assert(shop.addNewProduct({ id: "1", name: "Some Product1", producer: "Some Producer1" }));
-    assert(shop.addNewProduct({ id: "5", name: "Other Product5", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "6", name: "Other Product6", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "7", name: "Other Product7", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "8", name: "Other Product8", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "9", name: "Other Product9", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "10", name: "Other Product10", producer: "Other Producer4" }));
-    assert(shop.addNewProduct({ id: "11", name: "Other Product11", producer: "Other Producer4" }));
-    var byNames = shop.listProductsByName("Product");
-    assert(byNames.length == 10);
-    byNames = shop.listProductsByName("Some Product");
-    assert(byNames.length == 4);
-    assert(byNames.indexOf("Some Producer3 - Some Product1") >= 0);
-    assert(byNames.indexOf("Some Product2") >= 0);
-    assert(byNames.indexOf("Some Product3") >= 0);
-    assert(byNames.indexOf("Some Product1") < 0);
-    assert(byNames.indexOf("Some Producer1 - Some Product1") >= 0);
-    var byProducer = shop.listProductsByProducer("Producer");
-    assert(byProducer.length == 10);
-    byProducer = shop.listProductsByProducer("Some Producer");
-    assert(byProducer.length == 4);
-    assert(byProducer[0] == "Some Product1");
-    assert(byProducer[1] == "Some Product2" || byProducer[1] == "Some Product3");
-    assert(byProducer[2] == "Some Product2" || byProducer[2] == "Some Product3");
-    assert(byProducer[3] == "Some Product1");
+  assert(!shop.deleteProduct("1"));
+  assert(shop.addNewProduct({ id: "1", name: "1", producer: "Lex" }));
+  assert(!shop.addNewProduct({ id: "1", name: "any name because we check id only", producer: "any producer" }));
+  assert(shop.deleteProduct("1"));
+  assert(shop.addNewProduct({ id: "3", name: "Some Product3", producer: "Some Producer2" }));
+  assert(shop.addNewProduct({ id: "4", name: "Some Product1", producer: "Some Producer3" }));
+  assert(shop.addNewProduct({ id: "2", name: "Some Product2", producer: "Some Producer2" }));
+  assert(shop.addNewProduct({ id: "1", name: "Some Product1", producer: "Some Producer1" }));
+  assert(shop.addNewProduct({ id: "5", name: "Other Product5", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "6", name: "Other Product6", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "7", name: "Other Product7", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "8", name: "Other Product8", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "9", name: "Other Product9", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "10", name: "Other Product10", producer: "Other Producer4" }));
+  assert(shop.addNewProduct({ id: "11", name: "Other Product11", producer: "Other Producer4" }));
+  var byNames = shop.listProductsByName("Product");
+  assert(byNames.length == 10);
+  byNames = shop.listProductsByName("Some Product");
+  assert(byNames.length == 4);
+  assert(byNames.indexOf("Some Producer3 - Some Product1") >= 0);
+  assert(byNames.indexOf("Some Product2") >= 0);
+  assert(byNames.indexOf("Some Product3") >= 0);
+  assert(byNames.indexOf("Some Product1") < 0);
+  assert(byNames.indexOf("Some Producer1 - Some Product1") >= 0);
+  var byProducer = shop.listProductsByProducer("Producer");
+  assert(byProducer.length == 10);
+  byProducer = shop.listProductsByProducer("Some Producer");
+  assert(byProducer.length == 4);
+  assert(byProducer[0] == "Some Product1");
+  assert(byProducer[1] == "Some Product2" || byProducer[1] == "Some Product3");
+  assert(byProducer[2] == "Some Product2" || byProducer[2] == "Some Product3");
+  assert(byProducer[3] == "Some Product1");
 }
 
 function assert(condition) {
